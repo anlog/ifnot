020c9d47076250662837b680d775464b8a76d14b
hello <hello@ifnot.cc>
2020-07-11 10:15:34 +0800

    tid
    Signed-off-by: hello <hello@ifnot.cc>

--- main.c
+++ main.c
@@ -10,10 +10,21 @@ __noreturn void _exit(int __status);
 
 int main() {
     int ret = sched_yield();
-    char buf[3];
-    buf[0] = '=';
-    buf[1] = 0x30 + ret;
-    buf[2] = '\0';
+    short tid = syscall(__NR_gettid);
+    // max 65536
+    int i = 0, t = tid;
+    int s[5];
+    while(t != 0) {
+        i++; s[i] = t % 10; t = t /10;
+    }
+
+    char buf[2 + i];
+    buf[0] = 0x30 + ret;
+    buf[1] = '-';
+    int j = 0;
+    while(i >= 0) {
+        buf[2 + j++] = 0x30 + s[i--];
+    }
     write(1, buf, sizeof(buf));
 
     //syscall(__NR_write, 1, "abc", 3);
